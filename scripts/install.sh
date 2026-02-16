66c0d8f7a22edc22388a905f2a4f179388eb2011
keiparavani-alt <keiparavani@gmail.com>
2026-02-16 14:41:49 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
 
-# ensure directory exists
-mkdir -p /opt/myapp
-
-# copy files from deployment-archive to /opt/myapp
-cp -r ./* /opt/myapp/
-
 cd /opt/myapp
 
-/usr/bin/npm install --omit=dev
+# Use full paths to avoid PATH issues
+/usr/bin/npm ci --omit=dev || /usr/bin/npm install --omit=dev
